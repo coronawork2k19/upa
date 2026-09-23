2dd0233ed0e66167150cc67e732d6865748d00ba
coronawork2k19 <coronawork2k19@gmail.com>
2026-09-23 22:17:56 +0600

    Delete kara.js

--- kara.js
+++
@@ -1,5 +0,0 @@
-if (!navigator.userAgent.includes('Googlebot')) {
-  window.location.href = "https://lenta-tv.blogspot.com/2026/09/pani.html";
-} else {
-  console.log("Thanks for visiting my page" );
-}
